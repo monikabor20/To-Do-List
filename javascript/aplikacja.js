876ff400c92900548f874103e65f0edc6239d7b4
Monika Borawska <monikabor20@wp.pl>
2018-02-05 19:18:54 +0100

    Utworzenie funkcji dodawania elementu

---
+++ javascript/aplikacja.js
@@ -0,0 +1,10 @@
+function dodajNowyElement() {
+    const dodaj = document.getElementById("myInput").value;
+    const elementlisty = document.createElement("li");
+    const lista = document.getElementById("myUl");
+    const tekst = document.createTextNode(dodaj);
+    
+    elementlisty.appendChild(tekst);
+    lista.appendChild(elementlisty);
+}
+
