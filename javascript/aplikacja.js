1bec5278bb57db9120a79cccc20c22560401dc37
Monika Borawska <monikabor20@wp.pl>
2018-02-12 20:00:38 +0100

    Dodanie check do elementów listy

--- javascript/aplikacja.js
+++ javascript/aplikacja.js
@@ -1,22 +1,16 @@
 function dodajNowyElement() {
-    const dodaj = document.getElementById("myInput").value;   
+    const dodaj = document.getElementById("myInput").value;
     const elementlisty = document.createElement("li");
     const lista = document.getElementById("myUl");
     const tekst = document.createTextNode(dodaj);
-    
+
     elementlisty.appendChild(tekst);
     lista.appendChild(elementlisty);
-    
+
     myInput.value = '';
+
+    const ikonkaCheck = document.createElement("span");
+    ikonkaCheck.className = "fas fa-check";
+
+    elementlisty.appendChild(ikonkaCheck);
 }
-
-
-
-
-
-
-
-
-
-
-
