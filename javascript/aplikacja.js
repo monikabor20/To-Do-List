464078325b4811cd60fd9d329ddb9438c48d5b2f
Monika Borawska <monikabor20@wp.pl>
2018-02-12 09:40:59 +0100

    Dodanie usunięcia treści z inputa po wyświetleniu zadania

--- javascript/aplikacja.js
+++ javascript/aplikacja.js
@@ -1,10 +1,22 @@
 function dodajNowyElement() {
-    const dodaj = document.getElementById("myInput").value;
+    const dodaj = document.getElementById("myInput").value;   
     const elementlisty = document.createElement("li");
     const lista = document.getElementById("myUl");
     const tekst = document.createTextNode(dodaj);
     
     elementlisty.appendChild(tekst);
     lista.appendChild(elementlisty);
+    
+    myInput.value = '';
 }
+
+
+
+
+
+
+
+
+
+
 
