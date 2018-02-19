8b2c8f9e5d84330af211992e9240c8d7168b8338
Monika Borawska <monikabor20@wp.pl>
2018-02-19 19:26:00 +0100

    Dodanie możliwości usunięcia elementu z listy

--- javascript/aplikacja.js
+++ javascript/aplikacja.js
@@ -11,6 +11,14 @@ function dodajNowyElement() {
 
     const ikonkaCheck = document.createElement("span");
     ikonkaCheck.className = "fas fa-check";
-
     elementlisty.appendChild(ikonkaCheck);
+    
+    const zamykanie = document.getElementsByClassName("fa-check");
+    for(i=0; i< zamykanie.length; i++){
+        zamykanie[i].onclick = function() {
+         const div = this.parentElement; 
+         div.style.display = "none";
+        }
+    }
+    
 }
